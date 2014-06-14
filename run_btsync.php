23941f1de70a114ccba603964e610e80ce76458d
FalseCAM <FalseCAM@googlemail.com>
2014-06-14 23:02:20 +0200

    btsync updates

--- run_btsync.php
+++ run_btsync.php
@@ -1,5 +1,5 @@
 <?php
-$connection = ldap_connect($'ldap://localhost');
+$connection = ldap_connect('ldap://localhost');
 ldap_set_option($connection, LDAP_OPT_PROTOCOL_VERSION, 3);
 $ldapBind = ldap_bind($connection,"cn=admin,dc=example,dc=org",'password');
 if (!$ldapBind)
@@ -20,11 +20,12 @@ for ($item = 0; $item < $results['count']; $item++)
   {
    $data = $results[$item][$attribute];
    $btsuser = "".$results[$item][$data][0]."";
+   $homedir = "";
    exec("getent passwd ".$btsuser." | cut -d: -f6", $homedir, $return_var);
    exec("pgrep -u ".$btsuser." btsync", $pid, $return_var);
-   $btsync_config=$homedir."/.btsync/config.json";
+   $btsync_config="".$homedir[0]."/.btsync/config.json";
    exec("start-stop-daemon -o -c ".$btsuser." -K -u ".$btsuser." -x $daemon");
-   exec("start-stop-daemon -b -o -c ".$btsuser." -S -u ".$btsuser." -x ".$daemon." --config ".$btsync_config);
+   exec("start-stop-daemon -b -o -c ".$btsuser." -S -u ".$btsuser." -x ".$daemon." -- --config ".$btsync_config);
   }
 }
 ?>  
