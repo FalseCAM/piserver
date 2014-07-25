0f4ca75f79e209a51345f0982bc8145a47427a35
FalseCAM <FalseCAM@googlemail.com>
2014-07-25 16:58:49 +0200

    bugfix

--- install_owncloud.sh
+++ install_owncloud.sh
@@ -38,47 +38,47 @@ a2enmod ssl
 
 
 #create owncloud apache config
-'<IfModule mod_ssl.c>' > /etc/apache2/sites-available/owncloud
-'<VirtualHost _default_:443>' >> /etc/apache2/sites-available/owncloud
-'        ServerName owncloud.${hostname}' >> /etc/apache2/sites-available/owncloud
-'        ServerAdmin webmaster@localhost' >> /etc/apache2/sites-available/owncloud
-'        DocumentRoot /var/www' >> /etc/apache2/sites-available/owncloud
-'       <Directory />' >> /etc/apache2/sites-available/owncloud
-'                Options FollowSymLinks' >> /etc/apache2/sites-available/owncloud
-'                AllowOverride None' >> /etc/apache2/sites-available/owncloud
-'        </Directory>' >> /etc/apache2/sites-available/owncloud
-'        <Directory /var/www/>' >> /etc/apache2/sites-available/owncloud
-'                Options Indexes FollowSymLinks MultiViews' >> /etc/apache2/sites-available/owncloud
-'                AllowOverride None' >> /etc/apache2/sites-available/owncloud
-'                Order allow,deny' >> /etc/apache2/sites-available/owncloud
-'                allow from all' >> /etc/apache2/sites-available/owncloud
-'        </Directory>' >> /etc/apache2/sites-available/owncloud
-'        ErrorLog ${APACHE_LOG_DIR}/error.log' >> /etc/apache2/sites-available/owncloud
-'        LogLevel warn' >> /etc/apache2/sites-available/owncloud
-'        CustomLog ${APACHE_LOG_DIR}/ssl_access.log combined' >> /etc/apache2/sites-available/owncloud
-'        SSLEngine on' >> /etc/apache2/sites-available/owncloud
-'        SSLCertificateFile    /etc/ssl/certs/piserver.crt' >> /etc/apache2/sites-available/owncloud
-'        SSLCertificateKeyFile /etc/ssl/private/piserver.key' >> /etc/apache2/sites-available/owncloud
-'        <FilesMatch "\.(cgi|shtml|phtml|php)$">' >> /etc/apache2/sites-available/owncloud
-'                SSLOptions +StdEnvVars' >> /etc/apache2/sites-available/owncloud
-'        </FilesMatch>' >> /etc/apache2/sites-available/owncloud
-'        <Directory /usr/lib/cgi-bin>' >> /etc/apache2/sites-available/owncloud
-'                SSLOptions +StdEnvVars' >> /etc/apache2/sites-available/owncloud
-'        </Directory>' >> /etc/apache2/sites-available/owncloud
-'        BrowserMatch "MSIE [2-6]" \\' >> /etc/apache2/sites-available/owncloud
-'                nokeepalive ssl-unclean-shutdown \\' >> /etc/apache2/sites-available/owncloud
-'                downgrade-1.0 force-response-1.0' >> /etc/apache2/sites-available/owncloud
-'        BrowserMatch "MSIE [17-9]" ssl-unclean-shutdown' >> /etc/apache2/sites-available/owncloud
-'        <Directory /var/www/owncloud>' >> /etc/apache2/sites-available/owncloud
-'                Options Indexes FollowSymLinks MultiViews' >> /etc/apache2/sites-available/owncloud
-'                AllowOverride All' >> /etc/apache2/sites-available/owncloud
-'                Order allow,deny' >> /etc/apache2/sites-available/owncloud
-'                Allow from all' >> /etc/apache2/sites-available/owncloud
-'				 Dav Off' >> /etc/apache2/sites-available/owncloud
-'                Satisfy Any' >> /etc/apache2/sites-available/owncloud
-'        </Directory>' >> /etc/apache2/sites-available/owncloud
-'</VirtualHost>' >> /etc/apache2/sites-available/owncloud
-'</IfModule>' >> /etc/apache2/sites-available/owncloud
+echo '<IfModule mod_ssl.c>' > /etc/apache2/sites-available/owncloud
+echo '<VirtualHost _default_:443>' >> /etc/apache2/sites-available/owncloud
+echo '        ServerName owncloud.${hostname}' >> /etc/apache2/sites-available/owncloud
+echo '        ServerAdmin webmaster@localhost' >> /etc/apache2/sites-available/owncloud
+echo '        DocumentRoot /var/www' >> /etc/apache2/sites-available/owncloud
+echo '       <Directory />' >> /etc/apache2/sites-available/owncloud
+echo '                Options FollowSymLinks' >> /etc/apache2/sites-available/owncloud
+echo '                AllowOverride None' >> /etc/apache2/sites-available/owncloud
+echo '        </Directory>' >> /etc/apache2/sites-available/owncloud
+echo '        <Directory /var/www/>' >> /etc/apache2/sites-available/owncloud
+echo '                Options Indexes FollowSymLinks MultiViews' >> /etc/apache2/sites-available/owncloud
+echo '                AllowOverride None' >> /etc/apache2/sites-available/owncloud
+echo '                Order allow,deny' >> /etc/apache2/sites-available/owncloud
+echo '                allow from all' >> /etc/apache2/sites-available/owncloud
+echo '        </Directory>' >> /etc/apache2/sites-available/owncloud
+echo '        ErrorLog ${APACHE_LOG_DIR}/error.log' >> /etc/apache2/sites-available/owncloud
+echo '        LogLevel warn' >> /etc/apache2/sites-available/owncloud
+echo '        CustomLog ${APACHE_LOG_DIR}/ssl_access.log combined' >> /etc/apache2/sites-available/owncloud
+echo '        SSLEngine on' >> /etc/apache2/sites-available/owncloud
+echo '        SSLCertificateFile    /etc/ssl/certs/piserver.crt' >> /etc/apache2/sites-available/owncloud
+echo '        SSLCertificateKeyFile /etc/ssl/private/piserver.key' >> /etc/apache2/sites-available/owncloud
+echo '        <FilesMatch "\.(cgi|shtml|phtml|php)$">' >> /etc/apache2/sites-available/owncloud
+echo '                SSLOptions +StdEnvVars' >> /etc/apache2/sites-available/owncloud
+echo '        </FilesMatch>' >> /etc/apache2/sites-available/owncloud
+echo '        <Directory /usr/lib/cgi-bin>' >> /etc/apache2/sites-available/owncloud
+echo '                SSLOptions +StdEnvVars' >> /etc/apache2/sites-available/owncloud
+echo '        </Directory>' >> /etc/apache2/sites-available/owncloud
+echo '        BrowserMatch "MSIE [2-6]" \\' >> /etc/apache2/sites-available/owncloud
+echo '                nokeepalive ssl-unclean-shutdown \\' >> /etc/apache2/sites-available/owncloud
+echo '                downgrade-1.0 force-response-1.0' >> /etc/apache2/sites-available/owncloud
+echo '        BrowserMatch "MSIE [17-9]" ssl-unclean-shutdown' >> /etc/apache2/sites-available/owncloud
+echo '        <Directory /var/www/owncloud>' >> /etc/apache2/sites-available/owncloud
+echo '                Options Indexes FollowSymLinks MultiViews' >> /etc/apache2/sites-available/owncloud
+echo '                AllowOverride All' >> /etc/apache2/sites-available/owncloud
+echo '                Order allow,deny' >> /etc/apache2/sites-available/owncloud
+echo '                Allow from all' >> /etc/apache2/sites-available/owncloud
+echo '				 Dav Off' >> /etc/apache2/sites-available/owncloud
+echo '                Satisfy Any' >> /etc/apache2/sites-available/owncloud
+echo '        </Directory>' >> /etc/apache2/sites-available/owncloud
+echo '</VirtualHost>' >> /etc/apache2/sites-available/owncloud
+echo '</IfModule>' >> /etc/apache2/sites-available/owncloud
 
 #restart apache
 a2ensite owncloud
