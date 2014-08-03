f27a9af039d4e830cdbfc661391ad42e3dd413c3
FalseCAM <FalseCAM@googlemail.com>
2014-08-03 18:31:59 +0200

    bugfix

--- configure_welcomescreen.sh
+++ configure_welcomescreen.sh
@@ -10,13 +10,13 @@ tput setaf 2 && echo 'configure console welcome screen' && tput setaf 7
 # Install figlet
   sudo apt-get install figlet
   
-echo -e 'figlet \"PiServer \"$(hostname)\"\"' >> /home/pi/.bash_profile
-echo -e 'systemup=$(uptime | awk -F \', lo\' \'{print $1}\' )' >> /home/pi/.bash_profile
-echo -e 'echo \"Uptime:$systemup\"' >> /home/pi/.bash_profile
-echo -e 'temperature=$(/opt/vc/bin/vcgencmd measure_temp | awk -F \'temp=\' \'{print $NF}\' | awk -F \"\'C\" \'{print $1}\')' >> /home/pi/.bash_profile
-echo -e 'echo \"Temperature: $temperature C\"' >> /home/pi/.bash_profile
-echo -e 'free -h' >> /home/pi/.bash_profile
-echo -e "df -h $(piserverfolder)" >> /home/pi/.bash_profile
+echo 'figlet "PiServer \"$(hostname)\""' >> /home/pi/.bash_profile
+echo "systemup=$(uptime | awk -F ', lo' '{print \$1}' )" >> /home/pi/.bash_profile
+echo 'echo \"Uptime:$systemup\"' >> /home/pi/.bash_profile
+echo "temperature=\$(/opt/vc/bin/vcgencmd measure_temp | awk -F \'temp=\' \'{print \$NF}\' | awk -F \"\'C\" \'{print \$1}\')" >> /home/pi/.bash_profile
+echo 'echo \"Temperature: \$temperature C\"' >> /home/pi/.bash_profile
+echo 'free -h' >> /home/pi/.bash_profile
+echo "df -h $(piserverfolder)" >> /home/pi/.bash_profile
 
 chown pi /home/pi/.bash_profile
 chgrp pi /home/pi/.bash_profile
